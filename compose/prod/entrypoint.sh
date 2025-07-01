e6acaadf61c6f6c0f525a1615f7af209f458178f
aslan19P <matackubovaslan@gmail.com>
2025-07-01 10:28:40 +0500

    guni fix

--- compose/prod/entrypoint.sh
+++ compose/prod/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn portfolio_backend.wsgi:application --bind 0.0.0.0:8003 --workers 4 --timeout 120 --forwarded-allow-ips="*" --log-level debug
+gunicorn portfolio_backend.wsgi:application --bind 0.0.0.0:8003 --workers 4 --timeout 120 --forwarded-allow-ips="*" --log-level debug --access-logfile -
