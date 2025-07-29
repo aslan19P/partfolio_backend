a243930a9b161cd93897b3be2d353e442dbf6496
aslan19P <matackubovaslan@gmail.com>
2025-07-29 14:05:41 +0500

    remove status

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,7 +6,6 @@ from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Home, CV, About, Skill, Project, Experience
 from .serializers import (
     HomeSerializer,
